0259158c6615e90e212ebc422e5afa3f572f57e5
Jonathan Gonzalez <queleimporta545@gmail,com>
2024-03-27 18:31:11 -0500

    terminada la clase 20 terminar lo de products y registrar un usuario

--- src/routers/api/sessions.router.api.js
+++ src/routers/api/sessions.router.api.js
@@ -1,29 +1,48 @@
 import { Router } from "express";
+import has8char from "../../middlewares/has8char.mid.js";
+import passport from "../../middlewares/passport.mid.js";
 
 const sessionsRouter = Router();
 
+//register
+sessionsRouter.post(
+  "/register",
+  has8char,
+  passport.authenticate("register", {
+    session: false,
+    failureRedirect: "/api/sessions/badauth",
+  }),
+  async (req, res, next) => {
+    try {
+      return res.json({
+        statusCode: 201,
+        message: "Registered!",
+      });
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
 //login
-sessionsRouter.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (email && password === "hola1234") {
-      req.session.email = email;
-      //el role se está hardcodeando, pero luego hay que buscarlo en mongo
-      //para saber el role real del usuario que inició sesión
-      req.session.role = "user"
+sessionsRouter.post(
+  "/login",
+  passport.authenticate("login", {
+    session: false,
+    failureRedirect: "/api/sessions/badauth",
+  }),
+  async (req, res, next) => {
+    try {
       return res.json({
         statusCode: 200,
         message: "Logged in!",
         session: req.session,
       });
+    } catch (error) {
+      return next(error);
     }
-    const error = new Error("Bad Auth");
-    error.statusCode = 401;
-    throw error;
-  } catch (error) {
-    return next(error);
   }
-});
+);
 
 //me
 sessionsRouter.post("/", async (req, res, next) => {
@@ -57,6 +76,18 @@ sessionsRouter.post("/signout", async (req, res, next) => {
       error.statusCode = 400;
       throw error;
     }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//badauth
+sessionsRouter.get("/badauth", (req, res, next) => {
+  try {
+    return res.json({
+      statusCode: 401,
+      message: "Bad auth",
+    });
   } catch (error) {
     return next(error);
   }
