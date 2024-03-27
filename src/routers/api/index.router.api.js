d6adc8fd6be5f1e71776370026f2b2f292bb2d4c
Jonathan Gonzalez <queleimporta545@gmail,com>
2024-03-27 13:54:20 -0500

    ya terminada la configuracion de la clase 18

--- src/routers/api/index.router.api.js
+++ src/routers/api/index.router.api.js
@@ -3,14 +3,18 @@ import usersRouter from "./users.router.api.js";
 // import eventsRouter from "./events.router.api.js";
 import productsRouter from "./products.router.api.js";
 import router from "./orders.router.api.js";
+// import cookiesRouter from "./cookies.router.api.js";
+import sessionsRouter from "./sessions.router.api.js";
 
 const apiRouter = Router()
 
 //definir los enrutadores de los recursos
 apiRouter.use("/users",usersRouter)
 // apiRouter.use("/events",eventsRouter)
+// apiRouter.use("/cookies", cookiesRouter);
 apiRouter.use("/products",productsRouter)
 apiRouter.use("/orders",router)
+apiRouter.use("/sessions", sessionsRouter);
 
 
 export default apiRouter
